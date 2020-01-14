98102fc4d12f5c89dd68fdfdb9ae0592a2bd1fec
GrimWaltzman <47303018+GrimWaltzman@users.noreply.github.com>
2020-01-14 20:55:05 +0200

    Finished basic auth functions

--- src/app/auth/auth-request.service.ts
+++ src/app/auth/auth-request.service.ts
@@ -7,9 +7,14 @@ import { HttpClient } from '@angular/common/http';
 export class AuthRequestService {
 
   private _registerUrl = 'http://localhost:3000/api/users/register';
+  private _logInUrl = 'http://localhost:3000/api/users/login';
   constructor(private http: HttpClient) { }
 
   registerUser(user){
     return this.http.post<any>(this._registerUrl, user);
+  }
+
+  logInUser(user){
+    return this.http.post<any>(this._logInUrl, user);
   }
 }
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { AuthRequestService } from '../auth-request.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  userData;
+  constructor(private fb: FormBuilder,
+    private ars: AuthRequestService) { }
 
   ngOnInit() {
+    this.userData = this.fb.group({
+      userEmail: ['', Validators.required],
+      userPassword: ['', Validators.required]
+    });
+  }
+
+  logInUser(submittedForm){
+    let user = {
+      email: submittedForm.value.userEmail,
+      password: submittedForm.value.userPassword
+    }
+
+    this.ars.logInUser(user)
+    .subscribe(
+      res=>console.log(res),
+      err=>console.log(err)
+    );
   }
 
 }
